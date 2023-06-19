6c62cf20fd527ad67c60aac2d1ed31582f26cc87
Emil Noren <stratosfloor@hotmail.com>
2023-06-19 17:06:18 +0200

    Add basic layout for question page

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,6 +11,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return (const Text('hej'));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Questions...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 3'),
+          )
+        ],
+      ),
+    );
   }
 }
